ad566fd3d363fa5c928c3486685b8d1bf0272692
Delbert <git@delbert.me>
2014-04-02 14:12:29 +0800

    Site updated at 2014-04-02 06:12:29 UTC

---
+++ code/ex11.rb
@@ -0,0 +1,8 @@
+print "How old are you?\t "
+age = gets.chomp()
+print "How tall are you? \t"
+height = gets.chomp()
+print "How much do you weigh? \t"
+weight = gets.chomp()
+
+puts "So, you're #{age} years old, #{height} meters tall and #{weight} kilograms heavy."
